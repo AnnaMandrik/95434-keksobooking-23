38c37476ee6a3429401584b65cc7de1e8016e07f
Анна <AnnaMandrik1984@yandx.ru>
2021-06-11 13:24:02 +0300

    Генерация данных

--- js/main.js
+++ js/main.js
@@ -23,3 +23,81 @@ function getRandomIntDrob (firstInteger, secondInteger, limitSigns) {
 
 }
 getRandomIntDrob(1.1, 2, 2);
+
+
+const shirota = getRandomIntDrob(35.65000, 35.70000, 5);
+const dolgota = getRandomIntDrob(139.70000, 139.80000, 5);
+const typeFlatArr = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkInOutArr = ['12:00', '13:00', '14:00'];
+const featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photosArr = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg' ];
+
+function findElems(anyNamedArray, counter) {
+  const newArray = [];
+  for (let index = 0; index < counter; index++) {
+    // находим случайное число
+    const newElemIndex = getRandomIntInclusive(0, anyNamedArray.length - 1);
+    // находим элемент по этому порядковому номеру
+    const newElem = anyNamedArray[newElemIndex];
+
+    // нужно сравнивнивать с самим собой
+    let povtoryBykv = [];
+    // заполняем массив всеми найденными повторами
+    povtoryBykv = newArray.filter((item) => {
+      if (item === newElem) {
+        return item = newElem;
+      }
+    });
+    // проверяем количество повторов (нет повторов это 0)
+
+    const thisElemExist = povtoryBykv.length;
+    // если повторов нет то добавляем элемент
+    if (!thisElemExist) {
+      newArray.push(newElem);
+    }
+  }
+  return newArray;
+}
+
+const createAuthorObject = () => {
+  let numberOfUser = getRandomIntInclusive(1, 8);
+  numberOfUser++;
+  return {
+    // eslint-disable-next-line no-useless-concat
+    avatar: `img/avatars/user 0${  numberOfUser }.png`,
+  };
+};
+
+
+const createOfferObject = () => ({
+  title: 'Сдается жильё!',
+  address: `{{location.${shirota}}}, {{location.${dolgota}}}`,
+  price: getRandomIntInclusive(1000, 5000) ,
+  type: typeFlatArr[getRandomIntInclusive(0, typeFlatArr.length-1)],
+  rooms: getRandomIntInclusive(1, 5),
+  guests: getRandomIntInclusive(1, 20),
+  checkin: checkInOutArr[getRandomIntInclusive(0, checkInOutArr.length-1)],
+  checkout: checkInOutArr[getRandomIntInclusive(0, checkInOutArr.length-1)],
+  features: findElems(featuresArr, featuresArr.length -1),
+  description: 'Уютная квартира, со всеми удобствами в центре города. Достопримечательности и магазины в пешей доступности. До метро 10 мин. пешком',
+  photos: findElems(photosArr,photosArr.length-1 ),
+});
+
+const createLocationObject = () => ({
+  lat: shirota,
+  lng: dolgota,
+});
+
+const SIMILAR_ANNOUNCMENT_COUNT = 10;
+
+const rentalAnnouncement =  ()=> ({
+  author:  createAuthorObject(),
+  offer: createOfferObject(),
+  location: createLocationObject(),
+});
+
+const similarAnnouncment = new Array(SIMILAR_ANNOUNCMENT_COUNT).fill(null).map(() => rentalAnnouncement());
+
+similarAnnouncment;
